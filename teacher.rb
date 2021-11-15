c45bbd55bed1ed2fe7935355e4c0163b614440ad
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-11-15 14:38:47 -0300

    feature: add teacher class that extends person with addition of @specialization iv and overriding of can_use_services?

---
+++ teacher.rb
@@ -0,0 +1,18 @@
+require_relative './person.rb'
+
+class Teacher < Person
+  def initialize(name: 'Unknown', age:, parent_permission: true, specialization:)
+    super(:name => name, :age => age, :parent_permission => parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+# Create a object with age < 18 and parent permission == false so if the method is correctly overridden the return should be true
+
+teacher = Teacher.new(:name => 'Matilda', :age => 17, :parent_permission => false, :specialization => 'math teacher')
+
+puts teacher.can_use_services?
