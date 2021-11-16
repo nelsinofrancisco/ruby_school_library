7682a731678af01b0b3d4b01b20e0052c5be3e33
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-11-16 15:59:10 -0300

    feature: add composition to class person

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
+require './corrector.rb'
+
 class Person
   def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   attr_accessor :name, :age
@@ -11,6 +14,10 @@ class Person
 
   def can_use_services?
     is_of_age? || @parent_permission
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
   end
 
   private
@@ -23,3 +30,11 @@ class Person
   end
   # rubocop:enable Naming/PredicateName
 end
+
+test = Person.new(name: 'zezinhotemumnomemuitogrande', age: 18)
+
+puts test.name
+
+test.validate_name
+
+puts test.name
