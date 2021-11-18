fd8d9fe7c421e7f4f55f28ee03bb3aca4c0e0518
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-11-18 11:46:28 -0300

    Featyre> add book class

---
+++ library/book.rb
@@ -0,0 +1,22 @@
+require './rental.rb'
+
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title:, author:)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def rent(date:, person:, rental: nil)
+    if rental 
+      @rentals.push(rental)
+    else
+      new_rental = Rental.new(date: date, book: self, person: person)
+      person.add_book(rental: new_rental)
+      @rentals.push(new_rental)
+    end
+  end
+end
